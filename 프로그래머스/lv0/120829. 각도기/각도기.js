d21f18aaa6da950d5813ebdfda990cc6f318cce3
cwonho <104820973+cwonho@users.noreply.github.com>
2022-12-31 18:44:17 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,12 @@
+function solution(angle) {
+    
+    if (0 < angle && angle < 90) {
+        return 1;
+    } else if (angle === 90) {
+        return 2;
+    } else if (angle > 90 && angle < 180) {
+        return 3;
+    } else {
+        return 4;
+    }
+}
