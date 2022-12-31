50b62d85f3f07277ac0e7fdaa8b346ca82d1e4df
cwonho <104820973+cwonho@users.noreply.github.com>
2022-12-31 18:48:13 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,12 +1,4 @@
 function solution(angle) {
     
-    if (0 < angle && angle < 90) {
-        return 1;
-    } else if (angle === 90) {
-        return 2;
-    } else if (angle > 90 && angle < 180) {
-        return 3;
-    } else {
-        return 4;
-    }
+   return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4
 }
