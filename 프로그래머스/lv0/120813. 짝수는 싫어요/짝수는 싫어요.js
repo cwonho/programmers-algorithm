79ab5e4d264f553c80d1297d7e980e6d3010d630
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-01 19:15:51 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,5 @@
 function solution(n) {
     let answer = [];
-    for (let i=1; i <= n; i++) {
-        if (i%2 !== 0) answer.push(i);
-    }
+    for (let i=1; i <= n; i+=2) answer.push(i);
     return answer;
 }
