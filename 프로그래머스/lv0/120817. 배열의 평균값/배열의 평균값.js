d90f5c4b738de60d7ecea1d2755cc2bbfbf2f2ce
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-02 03:24:11 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,5 +1,3 @@
 function solution(numbers) {
-    return numbers.reduce((acc, curr) => {
-        return acc + curr;
-    }, 0) / numbers.length;
+    return numbers.reduce((acc, curr) => acc + curr) / numbers.length;
 }
