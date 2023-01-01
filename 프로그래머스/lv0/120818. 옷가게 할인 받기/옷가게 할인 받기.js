602a52acb648dcc2c420b55d5a44b9c20094a4e7
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-02 04:20:22 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,13 +1,18 @@
 function solution(price) {
-    let answer=price;
+//     let answer=price;
     
-    if (price >= 500000) {
-        answer-=price*0.2;
-    } else if (price >= 300000) {
-        answer-=price*0.1;
-    } else if (price >= 100000) {
-        answer-=price*0.05;
-    }
+//     if (price >= 500000) {
+//         answer-=price*0.2;
+//     } else if (price >= 300000) {
+//         answer-=price*0.1;
+//     } else if (price >= 100000) {
+//         answer-=price*0.05;
+//     }
     
-    return parseInt(answer);
+//     return parseInt(answer);
+    
+    return price >= 500000 ? parseInt(price*0.8) :
+    price >= 300000 ? parseInt(price*0.9) :
+    price >= 100000 ? parseInt(price*0.95) :
+    parseInt(price);
 }
