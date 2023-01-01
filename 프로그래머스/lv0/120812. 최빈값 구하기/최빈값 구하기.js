7d0a4ac0cbed0902a079d1aaf07906e6aae192cf
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-01 18:54:19 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -2,24 +2,21 @@ function solution(array) {
     const newObj = {};
     
     array.forEach(num => {
-        if (newObj[num]) {
-            newObj[num] += 1;
-        } else {
-            newObj[num] = 1;
-        }
+        newObj[num] = ++newObj[num] || 1;
     })
     
     let modeKey = Object.keys(newObj)[0];
     let mode = newObj[modeKey];
     
-    for (const key in newObj) {
+    
+    for (let key in newObj) {
         if (newObj[key] > mode) {
             mode = newObj[key];
             modeKey = key;
         }
     }
     
-    for (const key in newObj) {
+    for (let key in newObj) {
         if (newObj[key] === mode && key !== modeKey) return -1;
     }
     
