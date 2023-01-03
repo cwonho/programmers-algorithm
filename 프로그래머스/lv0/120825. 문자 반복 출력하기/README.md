067f7cd6d49062235ba3f89e476bc1a634491523
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-03 11:45:31 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/README.md
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 반복 출력하기 - 120825 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.04 ms
+메모리: 33.6 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,11 +1,3 @@
 function solution(my_string, n) {
-    let result='';
-    
-    for (let i=0; i<my_string.length; i++) {
-        for (let j=1; j<=n; j++) {
-            result += my_string.charAt(i);
-        }
-    }
-    
-    return result;
+    return [...my_string].map(char => char.repeat(n)).join('');
 }
