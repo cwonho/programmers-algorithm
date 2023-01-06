fde9f5f412c20fef951cd15378b62b0aba219541
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-06 11:33:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,14 @@
+function solution(num_list, n) {
+    let result=[];
+    
+    for (let i=0; i < num_list.length;) {
+        const sliceN = [];
+        
+        for (let j=0; j<n; j++) {
+            sliceN.push(num_list[i])
+            i++;
+        }
+        result.push(sliceN);
+    }
+    return result;
+}
