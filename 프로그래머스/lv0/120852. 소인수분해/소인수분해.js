ca9a5022223e808d59d29e45c7d84b45b32994de
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-07 03:59:56 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let result=[];
+    let divisor=2;
+    
+    while (n >= 2) {
+        if (n%divisor === 0) {
+            result.push(divisor);
+            n /= divisor;
+        } else {
+            divisor++;
+        }
+    }
+    
+    return [...new Set(result)];
+}
