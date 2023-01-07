cb5e6a1cdb7d15219a79c4ed2b6972ce2c2c8899
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-07 17:32:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,10 +1,13 @@
 function solution(numbers) {
-    let max = Number.MIN_SAFE_INTEGER;
+//     let max = Number.MIN_SAFE_INTEGER;
     
-    for (let i=0; i<numbers.length-1; i++) {
-        for (let j=i+1; j<=numbers.length-1; j++) {
-            if (numbers[i]*numbers[j] > max) max = numbers[i]*numbers[j];
-        }
-    }
-    return max;
+//     for (let i=0; i<numbers.length-1; i++) {
+//         for (let j=i+1; j<=numbers.length-1; j++) {
+//             if (numbers[i]*numbers[j] > max) max = numbers[i]*numbers[j];
+//         }
+//     }
+//     return max;
+    numbers.sort((a, b) => b - a);
+    
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]);
 }
