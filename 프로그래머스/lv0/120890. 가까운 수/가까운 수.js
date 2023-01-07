22f41ce8beb17db97f4f95c854e76c513933665a
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-08 01:57:05 +0900

    [level 0] Title: 가까운 수, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,5 +1,3 @@
 function solution(array, n) {
-    const sorted = array.map(v=>[v, Math.abs(v-n)]).sort((a,b)=>a[1]-b[1]);
-    
-    return sorted[0][1] === sorted[1][1] ? Math.min(sorted[0][0], sorted[1][0]) : sorted[0][0];
+    return array.map(v=>[v, Math.abs(v-n)]).sort((a,b)=>a[1]-b[1]||a[0]-b[0])[0][0];
 }
