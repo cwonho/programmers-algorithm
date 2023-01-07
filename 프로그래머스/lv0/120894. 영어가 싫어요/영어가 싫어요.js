281c3aaf8837e18fa9616ee38cd01a599ec9d9dd
cwonho <104820973+cwonho@users.noreply.github.com>
2023-01-08 03:09:06 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,7 +2,7 @@ function solution(numbers) {
     num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     
     for (let i=0; i<num.length; i++) {
-        numbers = numbers.replaceAll(num[i], String(i));
+        numbers = numbers.replaceAll(num[i], i);
     }
     
     return Number(numbers);
