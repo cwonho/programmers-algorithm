2aa750a460967b86539e8176544c6659b4189e28
cwonho <cwonho16@gmail.com>
2023-02-05 18:29:51 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,10 @@
+function solution(chicken) {
+    let coupon = chicken
+    let service = 0
+
+    while(coupon >= 10) {
+        service+=coupon/10 << 0
+        coupon = coupon%10 + coupon/10 << 0
+    }
+    return service
+}
